aef9b874da3bf1f485a9a71f0d2627460aef6f4d
PAMinerva <indiealex@virgilio.it>
2023-04-16 16:09:07 +0200

    minor fix (comment)

--- samples/01C-VkHelloSCBs/src/VKHelloSCBs.cpp
+++ samples/01C-VkHelloSCBs/src/VKHelloSCBs.cpp
@@ -53,7 +53,7 @@ void VKHelloSCB::AllocateSCBs()
 
 void VKHelloSCB::PopulateSCBs()
 {
-    // SCBs are used in the context of a render pass ...
+    // SCBs are used in the context of a render pass instance ...
     VkCommandBufferBeginInfo cmdBufInfo = {};
     cmdBufInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
     cmdBufInfo.pNext = nullptr;
