30edad38e3655f7d93e8aaa4967946d26e1aa119
PAMinerva <indiealex@virgilio.it>
2023-04-20 15:43:12 +0200

    fix script

--- samples/01C-VkHelloSCBs/scripts/build.sh
+++ samples/01C-VkHelloSCBs/scripts/build.sh
@@ -14,11 +14,10 @@ defines="-DDEBUG -DVK_USE_PLATFORM_XLIB_KHR"
 links="-lX11 -lvulkan"
 
 echo Compiling shader...
-cd ../../bin
-glslangValidator -V ../samples/01C-VkHelloSCBs/data/shaders/triangle.vert -o ../samples/01C-VkHelloSCBs/data/shaders/triangle.vert.spv
-glslangValidator -V ../samples/01C-VkHelloSCBs/data/shaders/triangle.frag -o ../samples/01C-VkHelloSCBs/data/shaders/triangle.frag.spv
 
-cd ../samples/01C-VkHelloSCBs
+/../../bin/glslangValidator -V ./data/shaders/triangle.vert -o ./data/shaders/triangle.vert.spv
+/../../bin/glslangValidator -V ./data/shaders/triangle.frag -o ./data/shaders/triangle.frag.spv
+
 echo Building project...
 
 g++ -g src/*.cpp -o 01C-VkHelloSCBs.out $includes $defines $links
