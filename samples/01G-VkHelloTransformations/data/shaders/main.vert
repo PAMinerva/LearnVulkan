59405b058bbef54229fa8e0dc94de04f562b5d50
PAMinerva <indiealex@virgilio.it>
2023-06-20 13:08:47 +0200

    Hello Transformations

---
+++ samples/01G-VkHelloTransformations/data/shaders/main.vert
@@ -0,0 +1,23 @@
+#version 450
+
+layout (location = 0) in vec3 inPos;
+layout (location = 1) in vec4 inColor;
+
+layout(std140, set = 0, binding = 0) uniform buf {
+    mat4 View;
+    mat4 Projection;
+} uBuf;
+
+layout(std140, set = 0, binding = 1) uniform dynbuf {
+    mat4 World;
+} dynBuf;
+
+layout (location = 0) out vec4 outColor;
+
+void main() 
+{
+    outColor = inColor;                                  // Pass color to the next stage
+    vec4 worldPos = dynBuf.World * vec4(inPos, 1.0);     // Local to World
+    vec4 viewPos = uBuf.View * worldPos;                 // World to View
+    gl_Position = uBuf.Projection * viewPos;             // View to Clip
+}
---
+++ samples/01G-VkHelloTransformations/inc/VKHelloTransformations.hpp
@@ -0,0 +1,91 @@
+#pragma once
+
+#include "VKSample.hpp"
+#include "VKSampleHelper.hpp"
+
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include "glm/glm.hpp"
+
+class VKHelloTransformations : public VKSample
+{
+public:
+    VKHelloTransformations(uint32_t width, uint32_t height, std::string name);
+    ~VKHelloTransformations();
+
+    virtual void OnInit();
+    virtual void OnUpdate();
+    virtual void OnRender();
+    virtual void OnDestroy();
+
+    virtual void OnResize();
+
+private:
+    
+    void InitVulkan();
+    void SetupPipeline();
+    
+    void PopulateCommandBuffer(uint32_t currentImageIndex);
+    void SubmitCommandBuffer();
+    void PresentImage(uint32_t currentImageIndex);
+    
+    void CreateVertexBuffer();              // Create a vertex buffer
+    void CreateHostVisibleBuffers();        // Create a buffer in host-visible memory
+    void CreateHostVisibleDynamicBuffers(); // Create a dynamic buffer
+    void CreateDescriptorPool();            // Create a descriptor pool
+    void CreateDescriptorSetLayout();       // Create a descriptor set layout
+    void AllocateDescriptorSets();          // Allocate a descriptor set
+    void CreatePipelineLayout();            // Create a pipeline layout
+    void CreatePipelineObjects();           // Create a pipeline object
+
+    // Update buffer data
+    void UpdateHostVisibleBufferData();
+    void UpdateHostVisibleDynamicBufferData();
+
+    // For simplicity we use the same uniform block layout as in the vertex shader:
+    //
+    // layout(std140, set = 0, binding = 0) uniform buf {
+    //     mat4 View;
+    //     mat4 Projection;
+    // } uBuf;
+    //
+    // This way we can just memcopy the uBufVS data to match the uBuf memory layout.
+    // Note: You should use data types that align with the GPU in order to avoid manual padding (vec4, mat4)
+    struct {
+        glm::mat4 viewMatrix;         // 64 bytes
+        glm::mat4 projectionMatrix;   // 64 bytes
+    } uBufVS;
+
+    // Uniform block defined in the vertex shader to be used as a dynamic uniform buffer:
+    //
+    //layout(std140, set = 0, binding = 1) uniform dynbuf {
+    //     mat4 World;
+    // } dynBuf;
+    //
+    // Allow the specification of different world matrices for different objects by offsetting
+    // into the same buffer.
+    struct {
+        glm::mat4 *worldMatrix;        // pointer to an array of world matrices
+    } dynUBufVS;
+    
+    // Vertex layout used in this sample
+    struct Vertex {
+        glm::vec3 position;
+        glm::vec4 color;
+    };
+    
+    // Vertex and index buffers
+    struct {
+        VkDeviceMemory VBmemory; // Handle to the device memory backing the vertex buffer
+        VkBuffer VBbuffer;       // Handle to the Vulkan buffer object that the memory is bound to
+        VkDeviceMemory IBmemory; // Handle to the device memory backing the index buffer
+        VkBuffer IBbuffer;       // Handle to the Vulkan buffer object that the memory is bound to
+        size_t indexBufferCount; // Number of indices
+    } m_vertexindexBuffer;
+
+    // In this sample we have two draw calls for each frame.
+    const unsigned int m_numDrawCalls = 2;
+
+    // Sample members
+    float m_curRotationAngleRad;
+    size_t m_dynamicUBOAlignment;
+};
