4d8378fe80d53514aec5acf83115ba309f79487c
PAMinerva <indiealex@virgilio.it>
2023-06-22 10:04:14 +0200

    fix linux script

--- samples/01G-VkHelloTransformations/scripts/build.sh
+++ samples/01G-VkHelloTransformations/scripts/build.sh
@@ -7,7 +7,7 @@ else
     VULKAN_INCLUDE=../../external/include/vulkan/
 fi
 
-includes="-Iinc -I$VULKAN_INCLUDE"
+includes="-Iinc -I../../external/include/ -I$VULKAN_INCLUDE"
 
 defines="-DDEBUG -DVK_USE_PLATFORM_XLIB_KHR"
 
@@ -15,11 +15,11 @@ links="-lX11 -lvulkan"
 
 echo Compiling shader...
 
-/../../bin/glslangValidator -V -g ./data/shaders/triangle.vert -o ./data/shaders/triangle.vert.spv
-/../../bin/glslangValidator -V -g ./data/shaders/triangle.frag -o ./data/shaders/triangle.frag.spv
+/../../bin/glslangValidator -V -g ./data/shaders/main.vert -o ./data/shaders/main.vert.spv
+/../../bin/glslangValidator -V -g ./data/shaders/main.frag -o ./data/shaders/main.frag.spv
 
 echo Building project...
 
-g++ -g src/*.cpp -o 01E-VkHelloTextures.out $includes $defines $links
+g++ -g src/*.cpp -o 01G-VkHelloTransformations.out $includes $defines $links
 
 rm -f *.o
