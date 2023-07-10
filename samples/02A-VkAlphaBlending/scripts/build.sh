437971e14ebc2d6d5c9b36189f32f4a14e062e33
PAMinerva <indiealex@virgilio.it>
2023-07-10 13:21:59 +0200

    fix linux script

--- samples/02A-VkAlphaBlending/scripts/build.sh
+++ samples/02A-VkAlphaBlending/scripts/build.sh
@@ -16,10 +16,11 @@ links="-lX11 -lvulkan"
 echo Compiling shader...
 
 /../../bin/glslangValidator -V -g ./data/shaders/main.vert -o ./data/shaders/main.vert.spv
-/../../bin/glslangValidator -V -g ./data/shaders/main.frag -o ./data/shaders/main.frag.spv
+/../../bin/glslangValidator -V -g ./data/shaders/solid.frag -o ./data/shaders/solid.frag.spv
+/../../bin/glslangValidator -V -g ./data/shaders/interpolated.frag -o ./data/shaders/interpolated.frag.spv
 
 echo Building project...
 
-g++ -g src/*.cpp -o 01G-VkHelloTransformations.out $includes $defines $links
+g++ -g src/*.cpp -o 02A-VkAlphaBlending.out $includes $defines $links
 
 rm -f *.o
