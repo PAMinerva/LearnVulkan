6828ce0967689f9bb7433f3d6dc3c414b3f614dd
PAMinerva <indiealex@virgilio.it>
2023-11-15 16:19:19 +0100

    fix linux script

--- samples/02G-VkComputeParticles/scripts/build.sh
+++ samples/02G-VkComputeParticles/scripts/build.sh
@@ -7,26 +7,21 @@ else
     VULKAN_INCLUDE=../../external/include/vulkan/
 fi
 
-includes="-Iinc -I./external/include/ -I$VULKAN_INCLUDE"
-#-I../../external/include/
+includes="-Iinc -I../../external/include/ -I$VULKAN_INCLUDE"
+
 defines="-DDEBUG -DVK_USE_PLATFORM_XLIB_KHR"
 
 links="-lX11 -lvulkan"
 
 echo Compiling shader...
 
-./bin/glslangValidator -V -g ./data/shaders/render.vert -o ./data/shaders/render.vert.spv
-./bin/glslangValidator -V -g ./data/shaders/render.tesc -o ./data/shaders/render.tesc.spv
-./bin/glslangValidator -V -g ./data/shaders/render.tese -o ./data/shaders/render.tese.spv
-./bin/glslangValidator -V -g ./data/shaders/render.frag -o ./data/shaders/render.frag.spv
-
-#/../../bin/glslangValidator -V -g ./data/shaders/transformFeedback.vert -o ./data/shaders/transformFeedback.vert.spv
-#/../../bin/glslangValidator -V -g ./data/shaders/render.vert -o ./data/shaders/render.vert.spv
-#/../../bin/glslangValidator -V -g ./data/shaders/render.geom -o ./data/shaders/render.geom.spv
-#/../../bin/glslangValidator -V -g ./data/shaders/render.frag -o ./data/shaders/render.frag.spv
+/../../bin/glslangValidator -V -g ./data/shaders/render.vert -o ./data/shaders/render.vert.spv
+/../../bin/glslangValidator -V -g ./data/shaders/render.geom -o ./data/shaders/render.geom.spv
+/../../bin/glslangValidator -V -g ./data/shaders/render.frag -o ./data/shaders/render.frag.spv
+/../../bin/glslangValidator -V -g ./data/shaders/particle.comp -o ./data/shaders/particle.comp.spv
 
 echo Building project...
 
-g++ -g src/*.cpp -o 02E-VkTessellation.out $includes $defines $links
+g++ -g src/*.cpp -o 02G-VkComputeParticles.out $includes $defines $links
 
 rm -f *.o
